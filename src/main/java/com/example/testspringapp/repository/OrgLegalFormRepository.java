b110c0e9cac7c9566fefb25c1b4b7a5d2241cace
eugenevlasov <vlasov4991@yandex.ru>
2021-11-21 03:40:31 +0300

    Added wrapper "Optional" for the returned class OrgLegalForm

--- src/main/java/com/example/testspringapp/repository/OrgLegalFormRepository.java
+++ src/main/java/com/example/testspringapp/repository/OrgLegalFormRepository.java
@@ -3,7 +3,9 @@ package com.example.testspringapp.repository;
 import com.example.testspringapp.model.OrgLegalForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrgLegalFormRepository extends JpaRepository<OrgLegalForm, Long> {
 
-    OrgLegalForm findByIdent(String ident);
+    Optional<OrgLegalForm> findByIdent(String ident);
 }
